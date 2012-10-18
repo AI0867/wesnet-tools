45a88058583e1f7b8b5ed785a52fce9eaa5506e7
Alexander van Gessel (AI0867) <ai0867@gmail.com>
2012-10-19 00:46:36 +0200

    Simple wesnothd prober for valen

---
+++ wesnothd_probe.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import optparse
+import time
+
+import simplewml
+import wesnothd_client
+
+op = optparse.OptionParser("%prog [-s server] <versions>")
+
+op.add_option("-s", "--server", action = "append",
+    help = "Server(s) to probe")
+
+options, args = op.parse_args()
+
+servers = options.server
+if not servers:
+    servers = ["server.wesnoth.org"]
+versions = args
+if not versions:
+    versions = ["test"]
+
+for s in servers:
+    for v in versions:
+        try:
+            client = wesnothd_client.Client(server=s, version=v)
+            alive = True
+        except Exception as e:
+            alive = False
+        print "{0}-{1}={2:g}".format(s, v, alive)
+
