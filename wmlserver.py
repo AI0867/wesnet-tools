c0712c02da9b4f32cc6076abb2aa14972510eb21
Alexander van Gessel (AI0867) <ai0867@gmail.com>
2013-02-16 03:50:43 +0100

    Factor out some WML server stuff addond and wesnothd have in common to wmlserver.py

---
+++ wmlserver.py
@@ -0,0 +1,48 @@
+import gzip_connection
+import simplewml
+import time
+import traceback
+
+class WMLClient(object):
+    def __init__(self, sock):
+        self.sock = sock
+        self.wml = simplewml.SimpleWML()
+    def poll(self):
+        if self.sock.poll():
+            frag = self.sock.nextfragment()
+            if frag == None:
+                raise StopIteration
+            data = self.wml.parse(frag)
+            self.process(data)
+            return True
+        return False
+    def process(self, data):
+        raise NotImplementedError
+
+class WMLServer(object):
+    def __init__(self, clientclass, **kwargs):
+        self.clientclass = clientclass
+        self.sock = gzip_connection.GzipServer(**kwargs)
+        self.clients = []
+    def poll(self):
+        acted = False
+        if self.sock.poll():
+            self.accept(self.sock.accept())
+            acted = True
+        for client in self.clients:
+            try:
+                if client.poll():
+                    acted = True
+            except StopIteration:
+                self.clients.remove(client)
+            except Exception as e:
+                print "A client died:"
+                traceback.print_exc()
+                self.clients.remove(client)
+        return acted
+    def accept(self, sock):
+        self.clients.append(self.clientclass(sock))
+    def loop(self):
+        while True:
+            if not self.poll():
+                time.sleep(1)
