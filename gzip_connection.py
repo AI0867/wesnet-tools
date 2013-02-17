321a2db6dfbff6ae571c8966cf74c1ddd356fdd3
Alexander van Gessel (AI0867) <ai0867@gmail.com>
2013-02-17 03:55:26 +0100

    Add a nonblocking socket to gzip_connection

--- gzip_connection.py
+++ gzip_connection.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import errno
 import select
 import socket
 import struct
@@ -31,18 +32,76 @@ class GzipSocket(object):
         data = zlib.decompress(buf, 16+zlib.MAX_WBITS)
         return data
     def sendint(self, i):
-        return self.sock.send(struct.pack("!I", i))
+        self.sock.sendall(struct.pack("!I", i))
     def sendfragment(self, data):
         # Force gzip format. UNDOCUMENTED?! (and unreachable in zlib.compress)
         compressor = zlib.compressobj(9, zlib.DEFLATED, 16+zlib.MAX_WBITS, zlib.DEF_MEM_LEVEL, 0)
         buf = compressor.compress(data) + compressor.flush()
         res = self.sendint(len(buf))
-        return res + self.sock.send(buf)
+        self.sock.sendall(buf)
     def getpeername(self):
         return self.sock.getpeername()
 
+class GzipSocketNonBlocking(GzipSocket):
+    def __init__(self, *args, **kwargs):
+        GzipSocket.__init__(self, *args, **kwargs)
+        self.sock.setblocking(0)
+        self.readbuf = ""
+        self.writebuf = ""
+    def process(self, getpoll=False):
+        result = self.pollobj.poll(0)[0][1]
+        acted = False
+        if result & select.POLLOUT and self.writebuf:
+            sent = self.sock.send(self.writebuf)
+            self.writebuf = self.writebuf[sent:]
+            acted = True
+        if result & select.POLLIN:
+            self.readbuf += self.sock.recv(2**16)
+            acted = True
+        if result & (select.POLLERR | select.POLLNVAL):
+            raise socket.Error("poll returned POLLERR or POLLNVAL")
+        return result if getpoll else acted
+    def poll(self):
+        pollresult = self.process(True)
+        if pollresult & select.POLLHUP:
+            # You're not getting any more, so get the information to the caller
+            return True
+        if len(self.readbuf) < 4:
+            return False
+        length = struct.unpack("!I", self.readbuf[:4])[0]
+        return len(self.readbuf) >= 4 + length
+    def nextint(self):
+        pollresult = self.process(True)
+        if not self.readbuf and pollresult & select.POLLHUP:
+            return None
+        if len(self.readbuf) < 4:
+            raise socket.Error(errno.EWOULDBLOCK, "Not enough bytes read")
+        integer = struct.unpack("!I", self.readbuf[:4])[0]
+        self.readbuf = self.readbuf[4:]
+        return integer
+    def nextfragment(self):
+        length = self.nextint()
+        if length == None:
+            return None
+        if len(self.readbuf) < length:
+            raise socket.Error(errno.EWOULDBLOCK, "Not enough bytes read")
+        # Force gzip format. UNDOCUMENTED?!
+        data = zlib.decompress(self.readbuf[:length], 16+zlib.MAX_WBITS)
+        self.readbuf = self.readbuf[length:]
+        return data
+    def sendint(self, i):
+        self.writebuf += struct.pack("!I", i)
+        self.process()
+    def sendfragment(self, data):
+        # Force gzip format. UNDOCUMENTED?! (and unreachable in zlib.compress)
+        compressor = zlib.compressobj(9, zlib.DEFLATED, 16+zlib.MAX_WBITS, zlib.DEF_MEM_LEVEL, 0)
+        buf = compressor.compress(data) + compressor.flush()
+        res = self.sendint(len(buf))
+        self.writebuf += buf
+        self.process()
+
 class GzipServer(object):
-    def __init__(self, server="", port=15000):
+    def __init__(self, server="", port=15000, clientclass=GzipSocket):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind((server, port))
         self.sock.listen(1)
@@ -50,17 +109,16 @@ class GzipServer(object):
         self.pollobj.register(self.sock.fileno())
         self.clients = weakref.WeakValueDictionary()
         self.next_id = 1
-
+        self.clientclass = clientclass
     def poll(self):
         result = self.pollobj.poll(0)
         return len(result) > 0 and result[0][1] & select.EPOLLIN
     def accept(self):
         base_sock, _ = self.sock.accept()
-        wrapped_sock = GzipSocket(base_sock)
-        request = wrapped_sock.nextint()
+        request = GzipSocket(base_sock).nextint() # We need to block for this one
+        wrapped_sock = self.clientclass(base_sock)
         if request != 0:
             raise NotImplementedError("connection recovering not supported")
-        # synchronize?
         wrapped_sock.connection_num = self.next_id
         self.next_id += 1
         wrapped_sock.sendint(wrapped_sock.connection_num)
