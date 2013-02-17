e1242583abb732d349cf70f1b70cb88d84a07e3a
Alexander van Gessel (AI0867) <ai0867@gmail.com>
2013-02-17 04:17:49 +0100

    Convert recursion to iteration to prevent stackoverflows

--- simplewml.py
+++ simplewml.py
@@ -56,44 +56,47 @@ class SimpleWML(object):
         return self.next_until('=')
     def next_value(self):
         buf = ""
-        c = self.next_char()
-        if c == '_':
-            # We might skip something other than spaces, but that is what the server's simplewml does too
-            while c != '"':
-                c = self.next_char()
-        if c == '"':
-            endchar = '"'
-        else:
-            buf += c
-            endchar = '\n'
-        buf += self.next_until(endchar)
-        if endchar == '\n':
-            return buf
-        if self.has_next() and self.peek_char() == '"':
-            buf += '"'
-            buf += self.next_value()
-        elif self.has_next():
+        append_another = True
+        while append_another:
+            append_another = False
             c = self.next_char()
-            while True:
-                if c == '\n':
-                    break
-                elif c == ' ':
+            if c == '_':
+                # We might skip something other than spaces, but that is what the server's simplewml does too
+                while c != '"':
                     c = self.next_char()
-                elif c == '+':
-                    c = self.next_char()
-                    if c != '\n':
-                        raise Exception("No newline after concatenation operator\nString so far:\n{0}\nEntire tag:\n{1}".format(buf, self.wmlstring))
-                    #if should be enough, but some idiot might switch the textdomain twice
-                    while self.peek_char() == '#':
-                        line = self.next_until('\n')
-                        if not line.startswith("#textdomain "):
-                            raise Exception("Unknown comment-line: {0}".format(line))
-                    while self.peek_char().isspace():
-                        self.next_char()
-                    buf += self.next_value()
-                    break
-                else:
-                    raise Exception("No newline after end of attribute\nString so far:\n{0}\nEntire tag:\n{1}".format(buf, self.wmlstring))
+            if c == '"':
+                endchar = '"'
+            else:
+                buf += c
+                endchar = '\n'
+            buf += self.next_until(endchar)
+            if endchar == '\n':
+                return buf
+            if self.has_next() and self.peek_char() == '"':
+                buf += '"'
+                append_another = True
+            elif self.has_next():
+                c = self.next_char()
+                while True:
+                    if c == '\n':
+                        break
+                    elif c == ' ':
+                        c = self.next_char()
+                    elif c == '+':
+                        c = self.next_char()
+                        if c != '\n':
+                            raise Exception("No newline after concatenation operator\nString so far:\n{0}\nEntire tag:\n{1}".format(buf, self.wmlstring))
+                        #if should be enough, but some idiot might switch the textdomain twice
+                        while self.peek_char() == '#':
+                            line = self.next_until('\n')
+                            if not line.startswith("#textdomain "):
+                                raise Exception("Unknown comment-line: {0}".format(line))
+                        while self.peek_char().isspace():
+                            self.next_char()
+                        append_another = True
+                        break
+                    else:
+                        raise Exception("No newline after end of attribute\nString so far:\n{0}\nEntire tag:\n{1}".format(buf, self.wmlstring))
         return buf
     def parse_internal(self, tag):
         while True:
